9a9e4db28f36943f031de8a3a1904192ed52904c
bond1999 <46317327+bond1999@users.noreply.github.com>
2021-07-16 07:57:16 -0500

    CSV file being created, currently in (test mode) to get information from just one user. PGN needs to be iterated through.

--- request-data.py
+++ request-data.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import simplejson
+import datetime
+import random
+import os.path
 
 ## Edit these variables before running
 
@@ -17,24 +21,90 @@ MM = '06'
 
 ##
 
+# Global Variables
+
+numberOfPlayers = 0
+
 # Function to get List of players belonging to country/{CC}, CC is the Country Code
 def downloadListOfPlayers(CountryCode):
 	response = requests.get("https://api.chess.com/pub/country/" + CountryCode + "/players")
 	if(list(response.json().keys())[0] == "status"): 
 		print("Failed to download players from Country " + CountryCode + ". Please change the Country code!")
 	else:
-		data = json.dumps(response.json(), sort_keys = True, indent = 4)
-		json.dump(data, open('playerlist' + CountryCode + '.json', 'w'))
-		print("Sucessfully downloaded Currently Online players from " + CountryCode + ". JSON created!")
-	
+		json.dump(response.json(), open('playerlist' + CountryCode + '.json', 'w'))
+		print("Sucessfully downloaded " + str(len(response.json()['players'])) + " currently online players from " + CountryCode +"!")
+
 # Function to read the names of players from file and load stats for each
 def loadPlayerStats(filename):
-	file = open(filename,)
-	data = json.load(file)
-	for i in data['players']:
-		print(i) # change here
+	with open(filename) as file:
+		data = json.load(file)
+	
+	# for i in range(0, 50000):
+	# 	playerName = random.choice(data['players'])
+	for playerName in data['players']:
+		if(playerName == 'bond1999'):
+			global numberOfPlayers 
+			numberOfPlayers += 1
+		
+			# Get rating	
+			response = requests.get("https://api.chess.com/pub/player/" + playerName + "/stats")
+			stats = response.json()	
+			bestRating = 0
+			
+			# Uses primary game types to determine how good is a player and get a game based on those ratings
+			for category in stats.keys():
+				if category == 'chess_rapid' or category == 'chess_bullet' or category == 'chess_blitz':
+					if(stats.get(category)['last']['rating'] > 1000 and checkIfRecent(stats.get(category)['last']['date'])):
+						randomGame = getTotalChessGamesByMonth(playerName, YYYY, MM)
+
+			writeToCSV(0, playerName, randomGame, getPGN(randomGame['pgn']))
+
 	file.close();
-	# Only loads names yet
+
+# Function which writes data values directly to dataset.csv
+def writeToCSV(sn, playerName, gameDetails, pgn):
+	
+	# Tags
+	rated = gameDetails['rated']
+	gameUsername = playerName
+	gameURL = gameDetails['url']
+	gameRules = gameDetails['rules']
+	timeClass = gameDetails['time_class']
+	timeControl = gameDetails['time_control']
+	endTime = gameDetails['end_time']
+	whiteUsername = gameDetails['white']['username']
+	whiteRating = gameDetails['white']['rating']
+	whiteResult = gameDetails['white']['result']
+	blackUsername = gameDetails['black']['username']
+	blackRating = gameDetails['black']['rating']
+	blackResult = gameDetails['black']['result']
+	Result = gameDetails['white']['result']
+	ECO = 0
+	ECOURL = "nothing"
+	ECOName = "something"
+	Moves = 64
+	
+	# Array to store all the tags for a single game
+	Tags = [sn, rated, gameUsername, gameURL, gameRules, timeClass, timeControl, endTime, whiteUsername, 
+	whiteRating, whiteResult, blackUsername, blackRating, blackResult, Result, ECO, ECOURL, ECOName, Moves]
+	
+	if(os.path.isfile('data.csv')):
+		"Data being appended to file since file already exists"
+	else:
+		file = open('data.csv','w')
+		file.write("sn,rated,gameUsername,gameURL,gameRules,timeClass,timeControl,endTime,whiteUsername,whiteRating,whiteResult,blackUsername,blackRating,blackResult,Result,ECO,ECOURL,ECOName,Moves\n")
+		file.close()
+
+	printJSON(gameDetails)
+	print(Tags, len(Tags))
+
+	file = open('data.csv','a')
+	for tag in Tags:
+		file.write(str(tag) + ",")
+	file.write('\n')
+	file.close()
+
+
 
 # Function to parse and print JSON with Indentation
 def printJSON(obj):
@@ -46,24 +116,32 @@ def printJSON(obj):
 def getTotalChessGamesByMonth(player, YYYY, MM):
 	response = requests.get("https://api.chess.com/pub/player/" + player + "/games/" + YYYY + '/' + MM)
 	data = response.json()
-	# if(len(data['games']) > 120):
-	print(len(data['games']))
-	printJSON(data['games'][0])
-	getChessGame(data['games'][0]['url'])
-	# print(data['games'][0]['rules'])
+	print("%s had %s games" % (player, len(data['games'])))
 
-# Function to get a specific game's data
-def getChessGame(gameID):
-	response = requests.get(gameID)
-	print(response)
-	data = response
+	return random.choice(data['games'])
 	
+# Function to check if the last game was played since the defined YYYY and MM
+def checkIfRecent(time):
+	year = datetime.datetime.fromtimestamp(time).year
+	month = datetime.datetime.fromtimestamp(time).month
+	print(datetime.datetime.fromtimestamp(time))
+	if(int(YYYY) == year and int(MM) <= month):
+		return True
+
+# Function to get a specific game's data from PGN
+def getPGN(pgn):
+	file = open('game.txt','w')
+	file.write(pgn)
+	file.close()
+	file = open('game.txt','r')
+	print(file.read())
+	file.close()
 
 # Main function to run the program
 def main():
 	downloadListOfPlayers(CountryCode)
-	getTotalChessGamesByMonth('bond1999', YYYY, MM)
-
+	loadPlayerStats('playerlist' + CountryCode + '.json')
+	print('numberOfPlayers = ' + str(numberOfPlayers))
 
 # Run Program
 main()
