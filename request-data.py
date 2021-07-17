d7c3f2f07bfa9d1dc25bc613fcf827a87d1ce1f0
bond1999 <46317327+bond1999@users.noreply.github.com>
2021-07-16 21:44:23 -0500

    Project completed, Collecting Data

--- request-data.py
+++ request-data.py
@@ -3,8 +3,7 @@ import json
 import simplejson
 import datetime
 import random
-import os.path
-
+import os
 ## Edit these variables before running
 
 # Set this to get data from a country, then save output from calling the function and 
@@ -39,35 +38,53 @@ def loadPlayerStats(filename):
 	with open(filename) as file:
 		data = json.load(file)
 	
-	# for i in range(0, 50000):
-	# 	playerName = random.choice(data['players'])
-	for playerName in data['players']:
-		if(playerName == 'bond1999'):
-			global numberOfPlayers 
-			numberOfPlayers += 1
-		
-			# Get rating	
-			response = requests.get("https://api.chess.com/pub/player/" + playerName + "/stats")
-			stats = response.json()	
-			bestRating = 0
-			
-			# Uses primary game types to determine how good is a player and get a game based on those ratings
-			for category in stats.keys():
-				if category == 'chess_rapid' or category == 'chess_bullet' or category == 'chess_blitz':
-					if(stats.get(category)['last']['rating'] > 1000 and checkIfRecent(stats.get(category)['last']['date'])):
-						randomGame = getTotalChessGamesByMonth(playerName, YYYY, MM)
+	i = 0
+	if(os.path.isfile('data.csv')):
+		with open('data.csv', 'rb') as f:
+		    f.seek(-2, os.SEEK_END)
+		    while f.read(1) != b'\n':
+		        f.seek(-2, os.SEEK_CUR)
+		    last_line = f.readline().decode()
+		i = last_line.split(',')[0]
+		print("Data being appended to file already exists! Resuming operation..\n")
+	else:
+		file = open('data.csv','w')
+		file.write("sn, gameURL, rated, startTime, endTime, halfMoves, victoryReason, gameResult, winner, timeControl, whiteUsername, whiteRating, blackUsername, blackRating, moves, endingFEN, ECO, ECOURL, ECOName\n")
+		file.close()
+		print("New data.csv created! Starting Operation")	
 
-			writeToCSV(0, playerName, randomGame, getPGN(randomGame['pgn']))
+	for i in range(0, 300000):
+		playerName = random.choice(data['players'])	
+		global numberOfPlayers 
+		numberOfPlayers += 1
+	
+		# Get rating	
+		response = requests.get("https://api.chess.com/pub/player/" + playerName + "/stats")
+		stats = response.json()	
+		bestRating = 0
+		
+		# Uses primary game types to determine how good is a player and get a game based on those ratings
+		for category in stats.keys():
+			if category == 'chess_rapid' or category == 'chess_bullet' or category == 'chess_blitz':
+				if(stats.get(category)['last']['rating'] > 1600 and checkIfRecent(stats.get(category)['last']['date'])):
+					print(i, end = " ")
+					randomGame = getTotalChessGamesByMonth(playerName, YYYY, MM)
+					if(randomGame == "skip"):
+						print("skipped")
+					else:
+						writeToCSV(i, playerName, randomGame, getPGN(randomGame['pgn']))
+					break		
+		
 
 	file.close();
 
 # Function which writes data values directly to dataset.csv
 def writeToCSV(sn, playerName, gameDetails, pgn):
 	
-	# Tags
+	# Tags from gameDetails
 	rated = gameDetails['rated']
 	gameUsername = playerName
-	gameURL = gameDetails['url']
+	gameURL = gameDetails['url'][32:]
 	gameRules = gameDetails['rules']
 	timeClass = gameDetails['time_class']
 	timeControl = gameDetails['time_control']
@@ -79,25 +96,84 @@ def writeToCSV(sn, playerName, gameDetails, pgn):
 	blackRating = gameDetails['black']['rating']
 	blackResult = gameDetails['black']['result']
 	Result = gameDetails['white']['result']
+	
+	file = open('game.txt','r')
+	lines = [line.strip("\n") for line in file.readlines() if line != "\n"]
+	
+	gameResult = "0-0"
+	startTime = 0
 	ECO = 0
 	ECOURL = "nothing"
 	ECOName = "something"
-	Moves = 64
+	halfMoves = 64
+	endingFEN = "blank"
+	victoryReason = ""
+	moves = ""
+	for line in lines:
+		if(line[0] == '['):
+			line = line[1:len(line)-1]
+			data = line.split(" ", 1)
+			# Assigning data from PGN
+			if(data[0] == "Result"):
+				gameResult = data[1].strip('"')
+			if(data[0] == "CurrentPosition"):
+				endingFEN = data[1].strip('"')
+			if(data[0] == "ECO"):
+				ECO = data[1].strip('"')
+			if(data[0] == "ECOUrl"):
+				ECOURL = data[1].strip('"')
+				ECOName = data[1].strip('"')[31:]
+			if(data[0] == "Date"):
+				startTime = datetime.datetime.strptime(data[1].strip('"'), '%Y.%m.%d')
+			if(data[0] == "StartTime"):
+				startTime += datetime.timedelta(hours = int(data[1].strip('"').split(':')[0]))
+				startTime += datetime.timedelta(minutes = int(data[1].strip('"').split(':')[1]))
+				startTime += datetime.timedelta(seconds = int(data[1].strip('"').split(':')[2]))
+				startTime = startTime.timestamp()
+			if(data[0] == "EndDate"):
+				endTime = datetime.datetime.strptime(data[1].strip('"'), '%Y.%m.%d')
+			if(data[0] == "EndTime"):
+				endTime += datetime.timedelta(hours = int(data[1].strip('"').split(':')[0]))
+				endTime += datetime.timedelta(minutes = int(data[1].strip('"').split(':')[1]))
+				endTime += datetime.timedelta(seconds = int(data[1].strip('"').split(':')[2]))
+				endTime = endTime.timestamp()
+			if(data[0] == "Termination"):
+				victoryReason = data[1].strip('"')
+		
+		elif (line[0] == '1'):	
+			for i in range(0, len(line)):
+				if(line[i] == '.' and line[i+1] == ' ' and line[i-1] != '.'):
+					i += 2
+					while(line[i] != ' '):
+						moves += line[i]
+						i += 1
+					moves += " "
+				if(line[i] == '.' and line[i+1] == '.' and line[i+2] == '.' and line[i+3] == ' '):
+					i += 4
+					while(line[i] != ' '):
+						moves += line[i]
+						i += 1
+					moves += " "
+		
+		moves = moves[0:len(moves)-1]
+		halfMoves = len(moves.split(' '))
+
+				
+
 	
-	# Array to store all the tags for a single game
-	Tags = [sn, rated, gameUsername, gameURL, gameRules, timeClass, timeControl, endTime, whiteUsername, 
-	whiteRating, whiteResult, blackUsername, blackRating, blackResult, Result, ECO, ECOURL, ECOName, Moves]
-	
-	if(os.path.isfile('data.csv')):
-		"Data being appended to file since file already exists"
+
+	if(gameResult == "1-0"):
+		winner = "white"
+	elif(gameResult == "0-1"):
+		winner = "black"
 	else:
-		file = open('data.csv','w')
-		file.write("sn,rated,gameUsername,gameURL,gameRules,timeClass,timeControl,endTime,whiteUsername,whiteRating,whiteResult,blackUsername,blackRating,blackResult,Result,ECO,ECOURL,ECOName,Moves\n")
-		file.close()
+		winner = "draw"
 
-	printJSON(gameDetails)
-	print(Tags, len(Tags))
 
+	# Array to store all the tags for a single game
+	Tags = [sn, gameURL, rated, startTime, endTime, halfMoves, victoryReason, gameResult, winner, timeControl, 
+	whiteUsername, whiteRating, blackUsername, blackRating, moves, endingFEN, ECO, ECOURL, ECOName]
+	
 	file = open('data.csv','a')
 	for tag in Tags:
 		file.write(str(tag) + ",")
@@ -117,14 +193,15 @@ def getTotalChessGamesByMonth(player, YYYY, MM):
 	response = requests.get("https://api.chess.com/pub/player/" + player + "/games/" + YYYY + '/' + MM)
 	data = response.json()
 	print("%s had %s games" % (player, len(data['games'])))
-
-	return random.choice(data['games'])
+	if(len(data['games']) < 50):
+		return "skip"
+	else:
+		return random.choice(data['games'])
 	
 # Function to check if the last game was played since the defined YYYY and MM
 def checkIfRecent(time):
 	year = datetime.datetime.fromtimestamp(time).year
 	month = datetime.datetime.fromtimestamp(time).month
-	print(datetime.datetime.fromtimestamp(time))
 	if(int(YYYY) == year and int(MM) <= month):
 		return True
 
@@ -132,9 +209,6 @@ def checkIfRecent(time):
 def getPGN(pgn):
 	file = open('game.txt','w')
 	file.write(pgn)
-	file.close()
-	file = open('game.txt','r')
-	print(file.read())
 	file.close()
 
 # Main function to run the program
